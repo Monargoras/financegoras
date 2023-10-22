e27376cdd01e6e2fe4b50dbf74c8d2a87de00241
Felix Wagner <felixwagner99@googlemail.com>
2023-10-22 23:36:14 +0200

    add prettier, eslint, dashboard skeleton

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-	// Enable Vue to support Vue components.
-	integrations: [vue()],
+  // Enable Vue to support Vue components.
+  integrations: [vue(), tailwind()]
 });
