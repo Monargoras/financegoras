8e481f8eb42a226f0e5d783dd6253891782b0264
Felix Wagner <felixwagner99@googlemail.com>
2024-08-13 13:49:30 +0200

    change metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 import { theme } from '../theme';
 
 export const metadata = {
-  title: 'Mantine Next.js template',
-  description: 'I am using Mantine with Next.js!',
+  title: 'Financegoras',
+  description: 'Analyze your finances with ease!',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
