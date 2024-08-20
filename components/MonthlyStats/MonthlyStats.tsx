66ccb0159b208f3ac7c0a144379b2363acf4f498
Felix Wagner <felixwagner99@googlemail.com>
2024-08-20 22:53:56 +0200

    add percent of used money

--- components/MonthlyStats/MonthlyStats.tsx
+++ components/MonthlyStats/MonthlyStats.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import useSWR from 'swr'
-import { Container, Flex, Loader, Paper, Text } from '@mantine/core'
+import { Container, Flex, Loader, Paper, Text, useMantineTheme } from '@mantine/core'
 import { Dictionary } from '@/utils/types'
 
 interface MonthlyStatsProps {
@@ -9,6 +9,8 @@ interface MonthlyStatsProps {
 }
 
 export function MonthlyStats(props: MonthlyStatsProps) {
+  const theme = useMantineTheme()
+
   const fetcher = (input: RequestInfo | URL) => fetch(input).then((res) => res.json())
   const currentMonth = new Date().getMonth() + 1
   const currentYear = new Date().getFullYear()
@@ -20,17 +22,25 @@ export function MonthlyStats(props: MonthlyStatsProps) {
   return (
     <>
       {isLoading && <Loader color="blue" type="dots" />}
-      {error && <Text>Error loading data.</Text>}
+      {error && <Text>{props.dictionary.budgetPage.errorLoadingData}</Text>}
       {data && (
         <Container>
           <Flex gap="md" justify="space-apart" direction="row" wrap="wrap">
             <Paper shadow="sm" radius="md" withBorder p="md" m="xs">
               <Text>{props.dictionary.budgetPage.monthlyIncome}</Text>
-              <Text c="green">{data.totalIncome}€</Text>
+              <Text c={theme.colors.green[5]}>{data.totalIncome}€</Text>
             </Paper>
             <Paper shadow="sm" radius="md" withBorder p="md" m="xs">
               <Text>{props.dictionary.budgetPage.monthlyExpenses}</Text>
-              <Text c="red">{data.totalExpenses}€</Text>
+              <Text c={theme.colors.red[5]}>{data.totalExpenses}€</Text>
+              {
+                // calculate percentage of expenses from income
+                data.totalIncome > 0 ? (
+                  <Text c={theme.colors.red[5]}>{((data.totalExpenses / data.totalIncome) * 100).toFixed(2)}%</Text>
+                ) : (
+                  <Text c={theme.colors.red[5]}>100%</Text>
+                )
+              }
             </Paper>
           </Flex>
         </Container>
