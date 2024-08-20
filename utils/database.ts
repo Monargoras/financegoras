5274a5735790730a90b94c217f5c8f1a5c7d09b2
Felix Wagner <felixwagner99@googlemail.com>
2024-08-20 21:47:32 +0200

    fix bits being read to boolean correctly from db

--- utils/database.ts
+++ utils/database.ts
@@ -6,6 +6,13 @@ const dialect = new MysqlDialect({
   pool: createPool({
     uri: process.env.DB_URL,
     connectionLimit: 10,
+    // transform BIT(1) to boolean when reading from the database
+    typeCast: (field, next) => {
+      if (field.type === 'BIT' && field.length === 1) {
+        return field.buffer()![0] === 1
+      }
+      return next()
+    },
   }),
 })
 
