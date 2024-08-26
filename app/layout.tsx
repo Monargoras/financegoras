7086bc766cfef5049f275f1e4d589f8ad31c433e
Felix Wagner <felixwagner99@googlemail.com>
2024-08-26 16:23:58 +0200

    add not found root layout

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: 'Not Found - Financegoras',
+  description: 'Page not found',
+}
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" style={{ overflowX: 'hidden' }}>
+      <head>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no" />
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
