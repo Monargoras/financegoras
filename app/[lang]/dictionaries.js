0e7b977c287d394144806462bfa9771848ca5387
Felix Wagner <felixwagner99@googlemail.com>
2024-08-28 12:08:24 +0200

    fix console error when logging in

--- app/[lang]/dictionaries.js
+++ app/[lang]/dictionaries.js
@@ -5,4 +5,4 @@ const dictionaries = {
   de: () => import('../../dictionaries/de.json').then((module) => module.default),
 }
 
-export const getDictionary = async (locale) => dictionaries[locale]()
+export const getDictionary = async (locale) => (locale === 'en' ? dictionaries.en() : dictionaries.de())
