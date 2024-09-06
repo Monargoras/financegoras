7a16e30372e80d5bfec579dd4371e2fea018806c
Felix Wagner <felixwagner99@googlemail.com>
2024-09-07 01:00:51 +0200

    add icons for different platforms

--- middleware.ts
+++ middleware.ts
@@ -41,14 +41,21 @@ export function middleware(request: NextRequest) {
 
   // // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
   // // If you have one
-  // if (
-  //   [
-  //     '/manifest.json',
-  //     '/favicon.ico',
-  //     // Your other files in `public`
-  //   ].includes(pathname)
-  // )
-  //   return
+  if (
+    [
+      '/favicon.ico',
+      '/apple-touch-icon.png',
+      '/.webmanifest',
+      '/android-chrome-192x192.png',
+      '/android-chrome-512x512.png',
+      '/mstile-150x150.png',
+      '/browserconfig.xml',
+      '/favicon-16x16.png',
+      '/favicon-32x32.png',
+      // Your other files in `public`
+    ].includes(pathname)
+  )
+    return NextResponse.redirect(new URL(`/${pathname.startsWith('/') ? '' : '/'}${pathname}`, request.url))
 
   // Check if there is any supported locale in the pathname
   const pathnameIsMissingLocale = locales.every(
