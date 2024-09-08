3cbf6a9c1f209e2ffe11e7e215a286d3d9816258
Felix Wagner <felixwagner99@googlemail.com>
2024-09-08 23:29:01 +0200

    add data security info

--- components/Welcome/LandingIntro.tsx
+++ components/Welcome/LandingIntro.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import { Text, Flex, Paper, em } from '@mantine/core'
+import { Text, Flex, Paper, em, useMantineTheme } from '@mantine/core'
 import Image from 'next/image'
 import { useMediaQuery } from '@mantine/hooks'
+import { IconLock } from 'tabler-icons'
 import classes from './Welcome.module.css'
 import { Dictionary } from '@/utils/types'
 import FinancegorasImage from '@/assets/FinancegorasDashboard.png'
@@ -13,27 +14,44 @@ interface LandingIntroProps {
 
 export default function LandingIntro(props: LandingIntroProps) {
   const isMobile = useMediaQuery(`(max-width: ${em(1200)})`)
+  const theme = useMantineTheme()
 
   return (
-    <Paper w="max(min(60dvw, 1000px), 400px)" mx="auto" radius="50%">
-      <Flex direction="row" mt="xl" align="center" wrap="wrap" justify={isMobile ? 'center' : 'end'}>
-        <Text ta="center" maw="300" className={classes.text}>
-          {props.dictionary.landingPage.dashboardImageText}
-        </Text>
-        <Image
-          priority
-          src={FinancegorasImage}
-          alt="Financegoras Dashboard"
-          style={{
-            width: 'max(30dvw, 400px)',
-            height: 'max(25dvw, 325px)',
-            borderRadius: '10%',
-            display: 'block',
-            border: '3px solid #fff',
-            marginLeft: isMobile ? 0 : 24,
-          }}
-        />
-      </Flex>
-    </Paper>
+    <>
+      <Paper w="max(min(60dvw, 1000px), 400px)" mx="auto" radius="50%" mt="xl">
+        <Flex direction="row" align="center" wrap="wrap" justify={isMobile ? 'center' : 'end'}>
+          <Text ta="center" maw="300" className={classes.textLeft}>
+            {props.dictionary.landingPage.dashboardImageText}
+          </Text>
+          <Image
+            priority
+            src={FinancegorasImage}
+            alt="Financegoras Dashboard"
+            style={{
+              width: 'max(30dvw, 400px)',
+              height: 'max(25dvw, 325px)',
+              borderRadius: '10%',
+              display: 'block',
+              border: '3px solid #fff',
+              marginLeft: isMobile ? 0 : 24,
+            }}
+          />
+        </Flex>
+      </Paper>
+      <Paper w="max(min(60dvw, 1000px), 400px)" h="max(min(20dvh, 600px), 350px)" mx="auto" radius="50%" mt="xl">
+        <Flex direction={isMobile ? 'column' : 'row'} align="center" wrap="wrap" justify="center" h="100%">
+          <IconLock
+            color={theme.colors[theme.primaryColor][5]}
+            style={{
+              width: isMobile ? 'max(5dvw, 50px)' : 'max(10dvw, 100px)',
+              height: isMobile ? 'max(5dvw, 50px)' : 'max(10dvw, 100px)',
+            }}
+          />
+          <Text ta="center" maw={isMobile ? '300' : '400'} className={classes.textRight}>
+            {props.dictionary.landingPage.dataSecurityText}
+          </Text>
+        </Flex>
+      </Paper>
+    </>
   )
 }
