386afb5a5e6fa194be02382c931a018875b1b064
Felix Wagner <felixwagner99@googlemail.com>
2024-09-09 17:53:47 +0200

    remove root layer layout and not found page to prevent hydration warnings

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,6 +1,11 @@
-import { MantineProvider } from '@mantine/core'
+import { ColorSchemeScript, MantineProvider } from '@mantine/core'
 import { Notifications } from '@mantine/notifications'
 import { getServerSession } from 'next-auth'
+import '@/app/[lang]/globals.css'
+import '@mantine/core/styles.css'
+import '@mantine/notifications/styles.css'
+import '@mantine/dates/styles.css'
+import '@mantine/charts/styles.css'
 import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions'
 import { theme } from '@/theme'
 import { Appbar } from '@/components/Appbar/Appbar'
@@ -41,6 +46,19 @@ export default async function RootLayout({
 
   return (
     <html lang={params.lang} style={{ overflowX: 'hidden' }}>
+      <head>
+        <ColorSchemeScript />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-icon.png" />
+        <link rel="shortcut icon" href="/img/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png" />
+        <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#339af0" />
+        <meta name="msapplication-TileColor" content="#2d89ef" />
+        <meta name="msapplication-config" content="/browserconfig.xml" />
+        <meta name="theme-color" content="#339af0" />
+      </head>
       <body>
         <MantineProvider theme={theme}>
           <ClientProviders session={session} language={params.lang}>
