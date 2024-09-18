ae704740b944433f49580703485f56cd36565e72
Felix Wagner <felixwagner99@googlemail.com>
2024-09-18 19:04:44 +0200

    fix year not requested correctly for month specific visualizations if not current year

--- app/api/budget/dashboard/route.ts
+++ app/api/budget/dashboard/route.ts
@@ -17,6 +17,7 @@ import getCategories from '../getCategories/getCategoriesAction'
  * @param month - the month of the year (optional)
  * @param selectedMonth - the selected month
  * @param year - the year
+ * @param selectedYear - the selected year
  * @param includeSavings - if true, the expenses are calculated with savings combined
  * @param lang - the current language used by user for month names
  * @param grouped - if true, the expenses are grouped by category group
@@ -35,25 +36,33 @@ export async function GET(request: NextRequest) {
   const monthString = request.nextUrl.searchParams.get('month')
   const selectedMonthString = request.nextUrl.searchParams.get('selectedMonth')
   const yearString = request.nextUrl.searchParams.get('year')
+  const selectedYearString = request.nextUrl.searchParams.get('selectedYear')
   const includeSavings = valueToBoolean(request.nextUrl.searchParams.get('includeSavings'))
   const grouped = valueToBoolean(request.nextUrl.searchParams.get('grouped'))
   const lang = request.nextUrl.searchParams.get('lang') ?? 'en'
 
-  if (!yearString || includeSavings === null || grouped === null || selectedMonthString === null) {
+  if (
+    !yearString ||
+    includeSavings === null ||
+    grouped === null ||
+    selectedMonthString === null ||
+    selectedYearString === null
+  ) {
     return new Response('More fields are required', { status: 400 })
   }
 
   const month = monthString ? parseInt(monthString, 10) : null
   const selectedMonth = parseInt(selectedMonthString, 10)
   const year = parseInt(yearString, 10)
+  const selectedYear = parseInt(selectedYearString, 10)
 
   // get all dashoard data sets as promises all
   const res = await Promise.all([
     getMonthlyExpenseEvolution(userId, year, month, lang, includeSavings, grouped),
     getIncExpEvolution(userId, year, month, lang),
-    getMonthlyData(userId, year, selectedMonth),
-    getExpensesByCategory(userId, year, selectedMonth, includeSavings, grouped),
-    getTransactions(userId, year, selectedMonth),
+    getMonthlyData(userId, selectedYear, selectedMonth),
+    getExpensesByCategory(userId, selectedYear, selectedMonth, includeSavings, grouped),
+    getTransactions(userId, selectedYear, selectedMonth),
     getCategories(userId),
   ])
 
