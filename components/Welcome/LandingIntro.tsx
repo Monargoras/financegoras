269625b42b4839b5da49c3079394781ebebd61f1
Felix Wagner <felixwagner99@googlemail.com>
2024-09-23 14:33:19 +0200

    keep intro boxes dark in light mode

--- components/Welcome/LandingIntro.tsx
+++ components/Welcome/LandingIntro.tsx
@@ -1,13 +1,12 @@
 'use client'
 
-import { Text, Flex, Paper, em, useMantineTheme, useMantineColorScheme } from '@mantine/core'
+import { Text, Flex, Paper, em, useMantineTheme } from '@mantine/core'
 import Image from 'next/image'
 import { useMediaQuery } from '@mantine/hooks'
 import { IconLock } from 'tabler-icons'
 import classes from './Welcome.module.css'
 import { Dictionary } from '@/utils/types'
 import FinancegorasImage from '@/assets/FinancegorasDashboard.png'
-import FinancegorasImageLight from '@/assets/FinancegorasDashboardLight.png'
 
 interface LandingIntroProps {
   dictionary: Dictionary
@@ -16,31 +15,37 @@ interface LandingIntroProps {
 export default function LandingIntro(props: LandingIntroProps) {
   const isMobile = useMediaQuery(`(max-width: ${em(1200)})`)
   const theme = useMantineTheme()
-  const { colorScheme } = useMantineColorScheme()
 
   return (
     <>
-      <Paper w="max(min(60dvw, 1000px), 400px)" mx="auto" radius="50%" mt="xl">
+      <Paper w="max(min(60dvw, 1000px), 400px)" mx="auto" radius="50%" mt="xl" bg={theme.colors.dark[7]}>
         <Flex direction="row" align="center" wrap="wrap" justify={isMobile ? 'center' : 'end'}>
-          <Text ta="center" maw="300" className={classes.textLeft}>
+          <Text ta="center" maw="300" className={classes.textLeft} c={theme.white}>
             {props.dictionary.landingPage.dashboardImageText}
           </Text>
           <Image
             priority
-            src={colorScheme === 'dark' ? FinancegorasImage : FinancegorasImageLight}
+            src={FinancegorasImage}
             alt="Financegoras Dashboard"
             style={{
               width: 'max(min(30dvw, 600px), 400px)',
               height: 'max(min(20dvw, 400px), 280px)',
               borderRadius: '10%',
-              display: 'block',
-              border: `3px solid ${colorScheme === 'dark' ? theme.white : theme.black}`,
+              border: `3px solid ${theme.white}`,
               marginLeft: isMobile ? 0 : 24,
+              display: 'block',
             }}
           />
         </Flex>
       </Paper>
-      <Paper w="max(min(60dvw, 1000px), 400px)" h="max(min(20dvh, 600px), 350px)" mx="auto" radius="50%" mt="xl">
+      <Paper
+        w="max(min(60dvw, 1000px), 400px)"
+        h="max(min(20dvh, 600px), 350px)"
+        mx="auto"
+        radius="50%"
+        mt="xl"
+        bg={theme.colors.dark[7]}
+      >
         <Flex direction={isMobile ? 'column' : 'row'} align="center" wrap="wrap" justify="center" h="100%">
           <IconLock
             color={theme.colors[theme.primaryColor][5]}
@@ -49,7 +54,7 @@ export default function LandingIntro(props: LandingIntroProps) {
               height: isMobile ? 'max(5dvw, 50px)' : 'max(10dvw, 100px)',
             }}
           />
-          <Text ta="center" maw={isMobile ? '300' : '400'} className={classes.textRight}>
+          <Text ta="center" maw={isMobile ? '300' : '400'} className={classes.textRight} c={theme.white}>
             {props.dictionary.landingPage.dataSecurityText}
           </Text>
         </Flex>
