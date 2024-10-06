3ffeded896102609bf7497c3f50b5de4344e7b8b
Felix Wagner <felixwagner99@googlemail.com>
2024-10-06 20:33:53 +0200

    add custom tooltip

--- components/AnalysisPage/CategoryEvolutionLineChart.tsx
+++ components/AnalysisPage/CategoryEvolutionLineChart.tsx
@@ -1,12 +1,57 @@
 'use client'
 
-import { useMatches } from '@mantine/core'
+import { Text, Paper, useMatches, Flex, useMantineColorScheme } from '@mantine/core'
 import { LineChart } from '@mantine/charts'
 import { CategoryEvolutionLineChartData, ColorMap } from '@/utils/types'
 
 interface CategoryEvolutionLineChartProps {
   data: CategoryEvolutionLineChartData
   colorMap: ColorMap
+}
+
+interface ChartTooltipProps {
+  label: string
+  payload: Record<string, unknown>[] | undefined
+}
+
+function ChartTooltip({ label, payload }: ChartTooltipProps) {
+  if (!payload) return null
+  const sortedPayload = [...payload]
+    .sort((a, b) => (b.value as number) - (a.value as number))
+    .filter((item) => (item.value as number) > 0)
+
+  const theme = useMantineColorScheme()
+  const textColor = theme.colorScheme === 'dark' ? 'white' : 'black'
+
+  return (
+    <Paper px="md" py="sm" withBorder shadow="md" radius="md">
+      <Text fw={500} mb={5} c={textColor}>
+        {label}
+      </Text>
+      <Flex gap={6} direction="column">
+        {sortedPayload.map((item) => (
+          <Flex key={item.name as string} align="center">
+            {(item.color as string) && (
+              <div
+                style={{
+                  width: 12,
+                  height: 12,
+                  borderRadius: 5,
+                  backgroundColor: item.color as string,
+                  marginRight: 10,
+                }}
+              />
+            )}
+            <Text fz="sm">{item.name as string}</Text>
+            <Flex ml={36} />
+            <Text ml="auto" fz="sm" c={textColor}>
+              {(item.value as number).toFixed(2)}€
+            </Text>
+          </Flex>
+        ))}
+      </Flex>
+    </Paper>
+  )
 }
 
 export default function CategoryEvolutionLineChart(props: CategoryEvolutionLineChartProps) {
@@ -42,9 +87,10 @@ export default function CategoryEvolutionLineChart(props: CategoryEvolutionLineC
       tooltipAnimationDuration={200}
       tooltipProps={{
         wrapperStyle: { zIndex: 1000 },
+        content: ({ label, payload }) => <ChartTooltip label={label} payload={payload as Record<string, number>[]} />,
       }}
       series={getSeries(props.data)}
-      valueFormatter={(value) => (value ? `${value.toFixed(2)}€` : '0€')}
+      valueFormatter={(value) => `${value.toFixed(2)}€`}
     />
   )
 }
