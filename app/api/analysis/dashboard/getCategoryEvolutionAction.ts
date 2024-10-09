ecc0da7d037ebeee4291b210a0070663bbd0c38b
Felix Wagner <felixwagner99@googlemail.com>
2024-10-09 15:22:54 +0200

    fix date filtering on analysis page graph

--- app/api/analysis/dashboard/getCategoryEvolutionAction.ts
+++ app/api/analysis/dashboard/getCategoryEvolutionAction.ts
@@ -6,28 +6,7 @@ import {
   getDynamicMonthYearTuples,
 } from '../../budget/getMonthlyExpenseEvolution/getMonthlyExpenseEvolutionUtils'
 import getUsedCategories from '../../budget/getCategories/getUsedCategoriesAction'
-
-function getTransactionsInMonth(
-  transactions: Transaction[],
-  month: number,
-  year: number,
-  startDate: Date,
-  endDate: Date
-) {
-  const curMonth = transactions.filter(
-    (transaction) =>
-      transaction.createdAt < new Date(year, month, 1) &&
-      (transaction.stoppedAt === null || transaction.stoppedAt >= new Date(year, month - 1, 1))
-  )
-  // filter for exakt day if month and year are either the start or end date
-  if (startDate.getMonth() + 1 === month && startDate.getFullYear() === year) {
-    return curMonth.filter((transaction) => transaction.createdAt >= startDate)
-  }
-  if (endDate.getMonth() + 1 === month && endDate.getFullYear() === year) {
-    return curMonth.filter((transaction) => transaction.createdAt <= endDate)
-  }
-  return curMonth
-}
+import { getTransactionsInMonth } from './analysisDashboardUtils'
 
 export default async function getCategoryEvolution(
   transactions: Transaction[],
@@ -46,13 +25,7 @@ export default async function getCategoryEvolution(
 
   const expenses = await Promise.all(
     monthsToCompute.map(([m, y]) =>
-      getCategoryEvolutionOneMonth(
-        m,
-        y,
-        lang,
-        getTransactionsInMonth(transactions, m, y, startDate, endDate),
-        filteredCategories
-      )
+      getCategoryEvolutionOneMonth(m, y, lang, getTransactionsInMonth(transactions, m, y), filteredCategories)
     )
   )
 
