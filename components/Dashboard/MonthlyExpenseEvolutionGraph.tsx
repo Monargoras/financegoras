48b27cd5523d5b65e359cf1f11944c9560c69c0a
Felix Wagner <felixwagner99@googlemail.com>
2024-11-02 11:59:59 +0100

    fix comment

--- components/Dashboard/MonthlyExpenseEvolutionGraph.tsx
+++ components/Dashboard/MonthlyExpenseEvolutionGraph.tsx
@@ -104,7 +104,6 @@ export default function MonthlyExpenseEvolutionGraph(props: MonthlyExpenseEvolut
       }}
       barProps={{
         onClick: (event) => {
-          // get month number from name
           // @ts-expect-error payload is not typed in correctly by recharts, but does exist
           const { month } = event.payload
           // get month number from name
