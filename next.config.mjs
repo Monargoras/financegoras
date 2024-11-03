f46b641a15c5550e49f81956e04fdd1518258cc3
Felix Wagner <felixwagner99@googlemail.com>
2024-11-03 12:04:47 +0100

    cleanup code

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
-import bundleAnalyzer from '@next/bundle-analyzer';
+import bundleAnalyzer from '@next/bundle-analyzer'
 
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
-});
+})
 
 export default withBundleAnalyzer({
   reactStrictMode: true,
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
-});
+})
