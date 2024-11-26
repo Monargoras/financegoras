d157e77839b74a9465c724acb861a2a9f7dd184c
Felix Wagner <felixwagner99@googlemail.com>
2024-11-26 23:49:55 +0100

    adapt locale switcher design to match other settings

--- components/Appbar/LocaleSwitcher.tsx
+++ components/Appbar/LocaleSwitcher.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import { Select } from '@mantine/core'
+import { ActionIcon, Menu, rem, Tooltip } from '@mantine/core'
 import { usePathname, useRouter } from 'next/navigation'
+import { IconCheck, IconLanguage } from '@tabler/icons-react'
 import { i18n, type Locale } from '@/utils/i18nConfig'
 import { Dictionary } from '@/utils/types'
 
@@ -22,17 +23,32 @@ export default function LocaleSwitcher(props: LocaleSwitcherProps) {
   }
 
   return (
-    <Select
-      data={i18n.locales.map((locale) => ({ value: locale, label: props.dictionary.general[locale] }))}
-      value={i18n.locales.find((locale) => pathName.startsWith(`/${locale}`))}
-      allowDeselect={false}
-      clearable={false}
-      w={100}
-      onChange={(value) => {
-        if (!value) return
-        router.push(redirectedPathName(value))
-        router.refresh()
-      }}
-    />
+    <Menu transitionProps={{ transition: 'pop' }}>
+      <Menu.Target>
+        <Tooltip label={props.dictionary.appbar.tooltipLanguageSwitch} position="bottom">
+          <ActionIcon variant="subtle" aria-label="change language menu" color="gray">
+            <IconLanguage />
+          </ActionIcon>
+        </Tooltip>
+      </Menu.Target>
+      <Menu.Dropdown>
+        {i18n.locales.map((locale) => (
+          <Menu.Item
+            key={locale}
+            leftSection={
+              i18n.locales.find((l) => pathName.startsWith(`/${l}`)) === locale && (
+                <IconCheck style={{ width: rem(16), height: rem(16) }} />
+              )
+            }
+            onClick={() => {
+              router.push(redirectedPathName(locale))
+              router.refresh()
+            }}
+          >
+            {props.dictionary.general[locale]}
+          </Menu.Item>
+        ))}
+      </Menu.Dropdown>
+    </Menu>
   )
 }
