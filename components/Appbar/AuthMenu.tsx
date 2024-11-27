31b9d841ef2109c66cd69e647706aeb2deed2aab
Felix Wagner <felixwagner99@googlemail.com>
2024-11-27 11:13:10 +0100

    change user menu to avatar style when logged in

--- components/Appbar/AuthMenu.tsx
+++ components/Appbar/AuthMenu.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, useMantineTheme } from '@mantine/core'
+import { Avatar, Box, Button, Popover, Tooltip, useMantineTheme } from '@mantine/core'
 import { useSession, signIn, signOut } from 'next-auth/react'
 import { useSWRConfig } from 'swr'
 import { Dictionary } from '@/utils/types'
@@ -18,33 +18,58 @@ export default function AuthMenu(props: AuthMenuProps) {
 
   return (
     <Box>
-      {!session && (
-        <Button
-          size="md"
-          onClick={() => signIn()}
-          variant={props.unstyled ? 'subtle' : 'filled'}
-          px={props.unstyled ? 16 : 'md'}
-          className={props.unstyled ? classes.control : undefined}
-        >
-          {props.dictionary.appbar.login}
-        </Button>
-      )}
-      {session && (
-        <Button
-          size="md"
-          c={theme.colors.red[5]}
-          color={theme.colors.red[5]}
-          variant="subtle"
-          onClick={() => {
-            signOut()
-            clearCache()
-          }}
-          px={props.unstyled ? 16 : 'md'}
-          className={classes.control}
-        >
-          {props.dictionary.appbar.logout}
-        </Button>
-      )}
+      <Tooltip
+        label={props.dictionary.budgetPage.fadedTransactionTooltip}
+        withArrow
+        multiline
+        maw={300}
+        visibleFrom="sm"
+      >
+        <Popover>
+          <Popover.Target>
+            <Box>
+              {!session && (
+                <Button
+                  size="md"
+                  onClick={() => signIn()}
+                  variant={props.unstyled ? 'subtle' : 'filled'}
+                  px={props.unstyled ? 16 : 'md'}
+                  className={props.unstyled ? classes.control : undefined}
+                >
+                  {props.dictionary.appbar.login}
+                </Button>
+              )}
+              {session && (
+                <Avatar
+                  src={session.user.image}
+                  alt={session.user.name ?? 'User Profile'}
+                  name={session.user.name ?? 'User Profile'}
+                  color="initials"
+                  style={{
+                    cursor: 'pointer',
+                  }}
+                />
+              )}
+            </Box>
+          </Popover.Target>
+          <Popover.Dropdown>
+            {session && (
+              <Button
+                size="md"
+                c={theme.colors.red[5]}
+                color={theme.colors.red[5]}
+                variant="subtle"
+                onClick={() => {
+                  signOut()
+                  clearCache()
+                }}
+              >
+                {props.dictionary.appbar.logout}
+              </Button>
+            )}
+          </Popover.Dropdown>
+        </Popover>
+      </Tooltip>
     </Box>
   )
 }
